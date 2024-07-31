a86539db74950b743f618769ec1c46d110e00ffb
RafaelConcellos <136651016+RafaelConcellos@users.noreply.github.com>
2024-07-31 01:12:18 +0000

    Added sidebar and slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.title("Hell yaa World!")
+st.header("Let's start it", divider="green")
+
+with st.sidebar:
+    st.header("About")
+    st.markdown('first time doing it')
+
+col1, col2 = st.columns(2)
+with col1:
+    x = st.slider("Choose your destiny", 1, 10)
+with col2:
+    st.write("flawless victory: ", x)
