97b480fed615e37f3db95f50fd311f625f100bbc
RafaelConcellos <136651016+RafaelConcellos@users.noreply.github.com>
2024-07-31 01:41:14 +0000

    Added table

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,13 @@ with col1:
     x = st.slider("Choose your destiny", 1, 10)
 with col2:
     st.write("flawless victory: ", x)
+
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
